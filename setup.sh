330dea9716941ee1d988c330cf8daeef1e01117a
inductor <kohei.ota@hpe.com>
2021-01-25 12:09:48 +0000

    Init repo

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if ! command -v COMMAND &> /dev/null
+then
+    echo "Docker not found, installing..."
+    sudo apt-get update
+
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+    sudo gpasswd -a $USER docker
+    echo "Restart shell to use Docker without sudo"
+    exit
+fi
+
+sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
